d1cabcb778530ccb8dc1ad822851dd3742faba03
HectorBecMe <hectorbecme@gmail.com>
2020-06-20 02:08:58 -0500

    chapter01 - 004
    Angular Refresher Demo13m 26s

---
+++ ng-architecture/src/app/todos/todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { TodosService } from './todos.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  title = 'Todos';
+  todos = ['Array Todo 1', 'Array Todo 2' , 'Array Todo 3'];
+
+  constructor(
+    private todosService: TodosService) {
+      this.todosService.getTodos().subscribe( data => {
+        console.log('TodosComponent -> data', data);
+        this.todos = data;
+      });
+    }
+
+  ngOnInit(): void {
+  }
+
+}
